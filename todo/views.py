c9e77e60b7c741de2cce1f5a5e5149986d219709
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-04 21:10:32 +0600

    add delate function

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import TodoItem
 
 # Create your views here.
@@ -8,3 +8,18 @@ def todoView(request):
     all_todo_items = TodoItem.objects.all()
     return render(request,'todo.html',
     {'all_items': all_todo_items})
+
+
+
+def addTodo(request):
+    
+    new_item = TodoItem(content = request.POST['content']) 
+    new_item.save()
+    return HttpResponseRedirect('/todo/')
+
+
+def deleteTodo(request,todo_id):
+    item_to_delete = TodoItem.objects.get(id=todo_id)
+    item_to_delete.delete()
+
+    return HttpResponseRedirect('/todo/')
--- todo_app/urls.py
+++ todo_app/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import todoView
+from todo.views import todoView,addTodo,deleteTodo;
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/' , todoView),
+    path('addTodo/',addTodo),
+     path('deleteTodo/<int:todo_id>/',deleteTodo),
 ]
+
